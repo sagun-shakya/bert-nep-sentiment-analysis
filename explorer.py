52e9138249fba40ed44d9aa60d253861ece2ebc9
sagun-shakya <sagun.shakya@naamii.org.np>
2022-03-20 12:09:20 +0545

    Added horizontal bar plot for test results.

--- explorer.py
+++ explorer.py
@@ -22,7 +22,8 @@ def parse_args():
     parser.add_argument('-o', '--target', 
                         type = str, metavar='PATH', default = r'./images/muril_trial_oyesh',
                         help = 'Path to the folder to store the images.')
-    
+    parser.add_argument('-t', '--plot_test', action = 'store_true', 
+                        help = 'Whether to display test results in a bar chart.')
     args = parser.parse_args()
     return args
 
@@ -48,7 +49,50 @@ def main():
         df = cache_df_dict[fold]
         df.drop(df[df['training loss'] < 1e-6].index, inplace = True)
         visualize_learning(df, save_loc = target, suffix = fold)
+    
+    if args.plot_test:
+        # Test results.
+        # Test filenames.
+        test_filenames = [os.path.join(source, file) for file in os.listdir(source) if file.startswith('test')]
+        assert len(test_filenames) > 0, "No test results in this directory."
         
+        # Column names : [testing accuracy, testing precision, testing recall, testing f1 score]
+        # Each result of the run is stored in a dictionary. Call cache_df_dict['fold3'], for example.
+        test_df_dict = {'fold' + str(ii) : pd.read_csv(t_file) for ii, t_file in enumerate(test_filenames, 1)}
+        
+        # Concat all the dataframes into one.
+        df = pd.concat(test_df_dict.values()).reset_index(drop = True)
+        
+        # Change the index names.
+        df.index = ['Fold ' + str(ii + 1) for ii in range(len(df))]
+        
+        # Color pallette.
+        colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
+        colors = colors[:df.shape[0]]
+        
+        # Plot horizontal barplot.
+        to_plot = df[["testing precision", "testing recall", "testing f1 score"]].T.round(3)
+        plot = to_plot.plot.barh(figsize = (15,8), width = 0.8, color = colors, xticks = np.arange(0,1.1,0.1))
+        
+        # Add grids on x-axis.
+        plot.xaxis.grid(True, linestyle = '-', linewidth = 0.25)
+        
+        # Add annotation.
+        for container in plot.containers:
+            plot.bar_label(container)
+            
+        # Filename of the plot generated.
+        out_filename = os.path.join(target, 'test_results.png')
+        
+        # Save file.
+        fig = plot.get_figure()
+        fig.savefig(out_filename)
+    
+    
+    
+
+    
+    
 # Driver code.
 if __name__ == "__main__":
     main()
